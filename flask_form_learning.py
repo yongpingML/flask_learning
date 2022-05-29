ea8a7ba53a731993e642199b3258694ded316338
YongpingChen <yongpingchen@yahoo.com>
2022-05-29 17:16:05 -0500

    Initial Commit

---
+++ flask_form_learning.py
@@ -0,0 +1,12 @@
+import math
+import os
+import sys
+
+import requests
+from flask_wtf import FlaskForm
+
+# print(sys.version)
+print(sys.executable)
+
+#r = requests.get('https://coreyms.com')
+#print(r.status_code)
